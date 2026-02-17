9aa947ecd61ad7befbbe500a7d32ccf7ca2f3cc5
girishmohakar <girishmohakarentc@gmail.com>
2026-02-18 01:16:04 +0530

    ArraySum

---
+++ ArraySum.java
@@ -0,0 +1,11 @@
+public class ArraySum {
+   public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        int total = 0;
+
+        for (int i=0 ; i<arr.length ; i++){
+            total = total +  arr[i];
+        }
+       System.out.println(total);
+    }
+}
